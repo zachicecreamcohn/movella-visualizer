77c1a74f83aceedd648484a8857f8208f4ac6e18
Zach Cohn <zcohn@wustl.edu>
2025-02-21 17:31:00 -0600

    first commit

---
+++ backend.js
@@ -0,0 +1,71 @@
+//import xsensManager, { PAYLOAD_TYPE } from "@vliegwerk/xsens-dot";
+import { Server } from "socket.io";
+
+import express from "express";
+import { createServer } from "node:http";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+//const payloadType = PAYLOAD_TYPE.completeEuler;
+
+app.use(express.static("./"));
+//xsensManager.on("dot", async (identifier) => {
+//  try {
+//    await xsensManager.connect(identifier);
+//    await xsensManager.subscribeMeasurement(identifier, payloadType);
+//    io.emit("registerNewSensor", { identifier });
+//  } catch (error) {
+//    console.error("Exception raised while connecting to Xsens DOT: ", error);
+//  }
+//});
+//
+//xsensManager.on("measurement", (identifier, data) => {
+//  console.log(`Measurement (${identifier}):`, data);
+//  io.emit("sensorData", { identifier, data });
+//});
+//
+//process.on("SIGINT", async () => {
+//  console.log("Disconnecting from all devices and exiting...");
+//  await xsensManager.disconnectAll();
+//  process.exit();
+//});
+
+function mockData() {
+  const data = {
+    timestamp: new Date().getTime(),
+    quaternion: {
+      w: Math.random(),
+      x: Math.random(),
+      y: Math.random(),
+      z: Math.random(),
+    },
+    freeAcceleration: {
+      x: Math.random(),
+      y: Math.random(),
+      z: Math.random(),
+    },
+  };
+  return data;
+}
+
+function setupMockData() {
+  io.emit("registerNewSensor", { identifier: "mock1" });
+  io.emit("registerNewSensor", { identifier: "mock2" });
+  io.emit("registerNewSensor", { identifier: "mock3" });
+  io.emit("registerNewSensor", { identifier: "mock4" });
+  setInterval(() => {
+    io.emit("sensorData", { identifier: "mock1", data: mockData() });
+    io.emit("sensorData", { identifier: "mock2", data: mockData() });
+    io.emit("sensorData", { identifier: "mock3", data: mockData() });
+    io.emit("sensorData", { identifier: "mock4", data: mockData() });
+  }, 700);
+}
+
+io.on("connection", (socket) => {
+  setupMockData();
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
---
+++ frontend.js
@@ -0,0 +1,126 @@
+const socket = io();
+
+/*
+ * `sensorData` will contain <sensor id>: object of TimeSeries that are updated with new data via socket.io
+ * {
+ *  <sensor id>: {
+ *  "freeAcceleration": {
+ *  "x": TimeSeries,
+ *  "y": TimeSeries,
+ *  "z": TimeSeries
+ *  },
+ *  "quaternion": {
+ *  "w": TimeSeries
+ *  "x": TimeSeries,
+ *  "y": TimeSeries,
+ *  "z": TimeSeries,
+ *  },
+ * }
+ */
+let sensorData = {};
+
+function addCanvas(identifier) {
+  const canvas = document.createElement("canvas");
+  canvas.setAttribute("sensor", identifier);
+  canvas.width = 500;
+  canvas.height = 200;
+  document.getElementById("canvas-container").appendChild(canvas);
+  return canvas;
+}
+
+function setupSmoothie(identifier) {
+  const canvas = addCanvas(identifier);
+  const smoothie = new SmoothieChart({
+    grid: {
+      strokeStyle: "#555555",
+      fillStyle: "#000000",
+    },
+  });
+
+  smoothie.streamTo(canvas, 1000);
+
+  sensorData[identifier] = {
+    freeAcceleration: {
+      x: new TimeSeries(),
+      y: new TimeSeries(),
+      z: new TimeSeries(),
+    },
+    quaternion: {
+      w: new TimeSeries(),
+      x: new TimeSeries(),
+      y: new TimeSeries(),
+      z: new TimeSeries(),
+    },
+  };
+
+  const freeAccelerationX = sensorData[identifier].freeAcceleration.x;
+  const freeAccelerationY = sensorData[identifier].freeAcceleration.y;
+  const freeAccelerationZ = sensorData[identifier].freeAcceleration.z;
+  const quaternionW = sensorData[identifier].quaternion.w;
+  const quaternionX = sensorData[identifier].quaternion.x;
+  const quaternionY = sensorData[identifier].quaternion.y;
+  const quaternionZ = sensorData[identifier].quaternion.z;
+
+  smoothie.addTimeSeries(freeAccelerationX, {
+    strokeStyle: "#0062ff",
+    lineWidth: 3,
+  });
+  smoothie.addTimeSeries(freeAccelerationY, {
+    strokeStyle: "#e600ff",
+    lineWidth: 3,
+  });
+  smoothie.addTimeSeries(freeAccelerationZ, {
+    strokeStyle: "#ff0000",
+    lineWidth: 3,
+  });
+  smoothie.addTimeSeries(quaternionW, { strokeStyle: "#ff9500", lineWidth: 3 });
+  smoothie.addTimeSeries(quaternionX, { strokeStyle: "#ffffff", lineWidth: 3 });
+  smoothie.addTimeSeries(quaternionY, { strokeStyle: "#4dff00", lineWidth: 3 });
+  smoothie.addTimeSeries(quaternionZ, { strokeStyle: "#00c8ff", lineWidth: 3 });
+
+  return smoothie;
+}
+
+function updateSensorData(identifier, data) {
+  console.log(data);
+  console.log(sensorData);
+  const freeAcceleration = data.freeAcceleration;
+  const quaternion = data.quaternion;
+
+  sensorData[identifier].freeAcceleration.x.append(
+    new Date().getTime(),
+    freeAcceleration.x,
+  );
+  sensorData[identifier].freeAcceleration.y.append(
+    new Date().getTime(),
+    freeAcceleration.y,
+  );
+  sensorData[identifier].freeAcceleration.z.append(
+    new Date().getTime(),
+    freeAcceleration.z,
+  );
+  sensorData[identifier].quaternion.w.append(
+    new Date().getTime(),
+    quaternion.w,
+  );
+  sensorData[identifier].quaternion.x.append(
+    new Date().getTime(),
+    quaternion.x,
+  );
+  sensorData[identifier].quaternion.y.append(
+    new Date().getTime(),
+    quaternion.y,
+  );
+  sensorData[identifier].quaternion.z.append(
+    new Date().getTime(),
+    quaternion.z,
+  );
+}
+
+socket.on("registerNewSensor", (data) => {
+  setupSmoothie(data.identifier);
+});
+
+socket.on("sensorData", (data) => {
+  updateSensorData(data.identifier, data.data);
+});
